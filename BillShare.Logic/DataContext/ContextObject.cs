7afc08eaf02c06bc2643c7145bc9952eefbd6f3f
Sandro6880 <31956997+Sandro6880@users.noreply.github.com>
2020-01-09 09:54:12 +0100

    Commit

---
+++ BillShare.Logic/DataContext/ContextObject.cs
@@ -0,0 +1,59 @@
+﻿//@BaseCode
+//MdStart
+using BillShare.Contracts;
+using BillShare.Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillShare.Logic.DataContext
+{
+    class ContextObject
+    {
+        internal abstract partial class ContextObject : IContext
+        {
+            public ContextObject()
+            {
+            }
+            #region Async-Methods
+            public abstract Task<int> CountAsync<I, E>()
+                where I : IIdentifiable
+                where E : IdentityObject, I;
+            public abstract Task<E> CreateAsync<I, E>()
+                where I : IIdentifiable
+                where E : IdentityObject, I, ICopyable<I>, new();
+            public abstract Task<E> InsertAsync<I, E>(I entity)
+                where I : IIdentifiable
+                where E : IdentityObject, ICopyable<I>, I, new();
+            public abstract Task<E> UpdateAsync<I, E>(I entity)
+                where I : IIdentifiable
+                where E : IdentityObject, I, ICopyable<I>, new();
+            public abstract Task<E> DeleteAsync<I, E>(int id)
+                where I : IIdentifiable
+                where E : IdentityObject, I;
+            public abstract Task SaveAsync();
+
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+            #endregion Async-Methods
+        }
+    }
+}
